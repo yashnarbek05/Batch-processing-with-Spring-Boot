8aff475f99bab306365424c3f804725583690f10
Tony <yashnarbeksattorov15@gmail.com>
2024-02-20 09:34:04 -0800

    first

---
+++ src/main/java/com/company/appbanch/AppBanchProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.company.appbanch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBanchProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBanchProcessingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/appbanch/AppBanchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.appbanch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBanchProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
